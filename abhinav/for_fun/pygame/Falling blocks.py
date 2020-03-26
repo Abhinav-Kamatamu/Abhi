a7010d237cab0701691ad7be317eb0b05eefe5ed
Abhinav Kamatamu <abhinav.kamatamu@gmail.com>
2020-03-26 19:14:08 +0530

    Changes

--- abhinav/for_fun/pygame/Falling blocks.py
+++ abhinav/for_fun/pygame/Falling blocks.py
@@ -10,10 +10,12 @@ h = w
 win = pygame.display.set_mode((w,h))
 pygame.display.set_caption('Abhinav made this Game')
 x = w//2-(w//10/2)
-y = h+10-(h//10/2)-h//10
+y = h+h//100-(h//10/2)-h//10
 s = w//10
 speed = w//200 + 4
 score = 0
+pcol = (0,255,0)
+run = True
 #----Block----
 T = [pygame.time.get_ticks() for i in range(5)]
 Bs = w//9
@@ -21,32 +23,33 @@ Bx = [random.randint(0, w - Bs) for _ in range(5)]
 By = [-Bs for _ in range(5)]
 S = [None for _ in range(5)]
 C = [random.randint(1,15) for _ in range(5)]
+bcol = (0,0,0)
 #----Block----
 #--variables--
 
 def draw():
     global win, x, y,s, player, speed
-    player = pygame.draw.rect(win,(255,0,0),(x,y,s,s))
+    player = pygame.draw.rect(win,pcol,(x,y,s,s))
     pygame.display.update()
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         if x<= 0:
             pass
         else:
-            x -= speed
+            x -= speed + 1
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         if x +Bs >= w:
             pass
         else:
-            x += speed
-    
+            x += speed + 1
+
 
 
 def block(i):
     global S,T,Bx,By,w,win,Bs,C,speed,score
     S[i-1] = pygame.time.get_ticks()
     if S[i-1]-T[i-1] >= C[i-1]*1000:
-        pygame.draw.rect(win,(0,0,255),(Bx[i-1],By[i-1],Bs,Bs))
+        pygame.draw.rect(win,bcol,(Bx[i-1],By[i-1],Bs,Bs))
         pygame.display.update()
         By[i-1] += speed
         if By[i-1] > w + Bs:
@@ -61,9 +64,9 @@ def block(i):
              for jx in range(Bx[i-1],Bx[i-1] + Bs):
                 for jy in range (By[i-1],By[i-1] + Bs):
                     if player.collidepoint(jx,jy):
-                        print(f'\nAbhinav\'s Game Says,\t\'Your score was {score}\'')
+                        print(f'\n\n\n\n\n\n\n\nAbhinav\'s Game Says,\t\'Your score was {score}\'')
                         exit()
-while True:
+while run:
     win.fill((255,255,255))
     draw()
     block(1)
@@ -73,7 +76,7 @@ while True:
     block(5)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(f'Abhinav\'s Game Says,\'Your score was {score}\'')
+            print(f'\n\n\n\n\n\n\n\nAbhinav\'s Game Says,\'Your score was {score}\'')
             exit()
     _type_=pygame.font.Font('freesansbold.ttf',(w*4)//100)
     text=_type_.render(f'Score:- {score}',True,(255,0,0))
@@ -82,4 +85,3 @@ while True:
     win.blit(text,textrect)
     pygame.display.update()
     clock.tick(fps)
-    
