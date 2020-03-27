430a16c069102917327ab3f7e860d485a1ee5985
Abhinav Kamatamu <abhinav.kamatamu@gmail.com>
2020-03-27 12:05:30 +0530

    UI changes in falling blocks

--- abhinav/for_fun/pygame/Collect the blocks.py
+++ abhinav/for_fun/pygame/Collect the blocks.py
@@ -6,15 +6,20 @@ ask = input('Do you know the rules?[Yes, no]')
 ask = ask.lower()
 
 if ask == "no":
-    print('-------Rules------\n The Rules are:- \n\tYou will have 2 minutes to play\n\tTry your best to collect the blocks\n\n-------Rules-------')
+    print('''
+----RULES----
+    
+    You will have 2 minutes to collect blocks
+    If your score reaches -5 you die
+''')
     time.sleep(5)
 #--variables--
 clock = pygame.time.Clock()
-w = int(input('\nSet screen size(500, 600, 700) prefered size is 500 :- '))
+w = 300
 fps = w//10
 h = w
 win = pygame.display.set_mode((w,h))
-pygame.display.set_caption('Abhinav made this Game')
+pygame.display.set_caption('Collect or die')
 x = w//2-(w//10/2)
 y = h+10-(h//10/2)-h//10
 s = w//10
@@ -23,12 +28,9 @@ score = 0
 checker = False
 timer = pygame.time.get_ticks()
 ender = 120000
-#---time up---
-cords = [(w//20,h//20),(),(),()]
-smalltype = pygame.font.Font('freesansbold.ttf',(w*4)//60)
-texts = [smalltype.render('TIME UP',True,(255,255,255)) for i in range(4)]
-textrects = [texts[i-1].get_rect() for i in range(4)]
+pcol = (0,255,0)
 #----Block----
+bcol = (0,0,0)
 T = [pygame.time.get_ticks() for i in range(5)]
 Bs = w//9
 Bx = [random.randint(0, w - Bs) for _ in range(5)]
@@ -39,28 +41,28 @@ C = [random.randint(1,15) for _ in range(5)]
 #--variables--
 
 def draw():
-    global win, x, y,s, player, speed
-    player = pygame.draw.rect(win,(255,0,0),(x,y,s,s))
+    global win, x, y,s, player, speed, pcol
+    player = pygame.draw.rect(win,pcol,(x,y,s,s))
     pygame.display.update()
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         if x<= 0:
             pass
         else:
-            x -= speed
+            x -= speed + 2
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         if x +Bs >= w:
             pass
         else:
-            x += speed
+            x += speed + 2
     
 
 
 def block(i):
-    global S,T,Bx,By,w,win,Bs,C,speed,score, checker
+    global S,T,Bx,By,w,win,Bs,C,speed,score, checker, bcol
     S[i-1] = pygame.time.get_ticks()
     if S[i-1]-T[i-1] >= C[i-1]*1000:
-        pygame.draw.rect(win,(0,0,255),(Bx[i-1],By[i-1],Bs,Bs))
+        pygame.draw.rect(win,bcol,(Bx[i-1],By[i-1],Bs,Bs))
         pygame.display.update()
         By[i-1] += speed
         if By[i-1] > w + Bs:
@@ -79,7 +81,10 @@ def block(i):
             score += 1
             checker = False
 while True:
-    win.fill((255,255,255))
+    if score > 50 and score < 100:
+        win.fill((random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+    else:
+        win.fill((255,255,255))
     draw()
     block(1)
     block(2)
@@ -88,7 +93,7 @@ while True:
     block(5)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(f'\nAbhinav\'s Game Says,\'Your score was {score}\'')
+            print(f'\n\n\n\n\nAbhinav\'s Game Says,\'Your score was {score}\'')
             exit()
     _type_=pygame.font.Font('freesansbold.ttf',(w*4)//100)
     text=_type_.render(f'Score:- {score}',True,(255,0,0))
@@ -107,10 +112,12 @@ while True:
             textrect=text.get_rect()
             textrect.center = (w//2,h//2)
             win.blit(text,textrect)
-            
             pygame.display.update()
             time.sleep(1)
         
-        print(f'\nAbhinav\'s Game Says,\'Your score was {score}\'')
+        print(f'\n\n\n\n\n\nAbhinav\'s Game Says,\'Your score was {score}\'')
         exit()
+    if score == -5:
+         print('\n\n\n\n\n\nAbhinav\'s Game Says,\'You died as your score was -5\'')
+         exit()
     clock.tick(fps)
