2e772a0e29897d9c8878c89f066977f46a2b04fa
Default user <abhinav.kamatamu@gmail.com>
2020-05-05 14:17:59 +0000

    Spot Clicker Added

---
+++ abhinav/for_fun/pygame/Spot_Clicker.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+#-------Init--------
+pygame.init()
+#-------Init--------
+#-----Variables-----
+w = 600
+h = 600
+win = pygame.display.set_mode((w.h))
+x,y = (1/10*w,9/10*h)
+Cx,Cy = (1/10*w,9/10*h)
+r = 20
+l,b = (8/10*w, 5/10
+r = 20
+l,b = (8/10*w, 5/100*h)
+#-----Variables-----
+
