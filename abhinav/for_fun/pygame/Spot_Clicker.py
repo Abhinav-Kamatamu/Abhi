cbaaed728b423fd26df9a58dd7305fd8d46ccbde
Abhinav Kamatamu <abhinav.kamatamu@gmail.com>
2020-05-19 12:54:50 +0530

    YeSgit add .

--- abhinav/for_fun/pygame/Spot_Clicker.py
+++ abhinav/for_fun/pygame/Spot_Clicker.py
@@ -6,7 +6,7 @@ pygame.init()
 
 #-----Variables-----
 w = 600
-h = 600
+h = w
 fps = 30
 win = pygame.display.set_mode((w,h))
 pygame.display.set_caption('Clicker')
@@ -14,28 +14,38 @@ x = int(0.1*w)
 y = int(0.9*h)
 cx = int(0.125*w)
 cy = int(0.925*h)
-r = 20
+r = int(1/30*w)
 l = int(0.8*w)
 b = int(0.05*h)
 run = True
+mx,my = 0,0
+points = 0
 clock = pygame.time.Clock()
 #-----Variables-----
 
 #-----Functions-----
+def pointing():
+    global mx,my
+    pass
 def click(x,y):
+    global points
     play.collidepoint(x,y)
+    points = pointing()
 def in_loops():
-    global run
-    for event in pygame.events.get():
-        if evet.type == pygame.QUIT:
+    global run,mx,my
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             run = False
+            pygame.quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mx,my = event.pos()
+            mx,my = event.pos
+            click(mx,my)
 #-----Functions-----
 while run:
     win.fill((255,255,255))
     pygame.draw.rect(win,(255,0,0),(x,y,l,b))
-    pygame.draw.circle(win,(0,0,0),(cx,cy),r,1)
+    pygame.draw.circle(win,(0,0,0),(cx,cy),r,4)
     pygame.display.update()
+    in_loops()
     clock.tick(fps)
 
