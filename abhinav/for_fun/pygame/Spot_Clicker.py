b74bdddb9485846fdbc6b07d4d676bf270ef3945
Default user <abhinav.kamatamu@gmail.com>
2020-05-19 07:06:58 +0000

    NO

--- abhinav/for_fun/pygame/Spot_Clicker.py
+++ abhinav/for_fun/pygame/Spot_Clicker.py
@@ -3,11 +3,13 @@ from pygame.locals import *
 #-------Init--------
 pygame.init()
 #-------Init--------
+
 #-----Variables-----
 w = 600
 h = 600
+fps = 30
 win = pygame.display.set_mode((w,h))
-pygame.display.set_caption('HI Game')
+pygame.display.set_caption('Clicker')
 x = int(0.1*w)
 y = int(0.9*h)
 cx = int(0.125*w)
@@ -15,10 +17,25 @@ cy = int(0.925*h)
 r = 20
 l = int(0.8*w)
 b = int(0.05*h)
+run = True
+clock = pygame.time.Clock()
 #-----Variables-----
-while True:
+
+#-----Functions-----
+def click(x,y):
+    play.collidepoint(x,y)
+def in_loops():
+    global run
+    for event in pygame.events.get():
+        if evet.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx,my = event.pos()
+#-----Functions-----
+while run:
     win.fill((255,255,255))
     pygame.draw.rect(win,(255,0,0),(x,y,l,b))
     pygame.draw.circle(win,(0,0,0),(cx,cy),r,1)
     pygame.display.update()
+    clock.tick(fps)
 
