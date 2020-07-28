011f7668bd5bbbc0e896ea3a02c6b79194f1d719
Abhinav <abhinav.kamatamu@gmail.com>
2020-07-28 22:27:33 +0530

    Changed to game

--- abhinav/for_fun/python/pygame/Game.py
+++ abhinav/for_fun/python/pygame/Game.py
@@ -1,15 +1,30 @@
 import random as r
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
 w = 500
 h = 500
 y = 0
+fps = 90
+clock = pygame.time.Clock()
 x = r.randint(0, w)
 win = pygame.display.set_mode((w, h))
+pygame.display.set_caption('Abhinav\'s PROGRAM')
 win.fill((255, 255, 255))
 pygame.display.update()
+
+
+def escape():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    keys = pygame.key.get_pressed()
+    if keys[K_ESCAPE]:
+        exit()
+
+
 while True:
     ob = pygame.draw.circle(win, (0, 0, 0), (x, y), 4)
     y += 1
@@ -18,3 +33,5 @@ while True:
         x = r.randint(0, w)
     pygame.display.update()
     win.fill((255, 255, 255))
+    escape()
+    clock.tick(fps)
