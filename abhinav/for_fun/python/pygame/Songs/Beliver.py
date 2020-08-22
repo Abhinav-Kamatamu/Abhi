09619609fdd333ec5c30ea213c5fe8846dddcc8b
Abhinav <abhinav.kamatamu@gmail.com>
2020-08-22 19:36:24 +0530

    memory.py

--- abhinav/for_fun/python/pygame/Songs/Beliver.py
+++ abhinav/for_fun/python/pygame/Songs/Beliver.py
@@ -1,9 +1,12 @@
 import pygame
 from pygame.locals import *
+
 pygame.init()
 pygame.mixer.init()
 pygame.mixer_music.load("Song.mp3")
-play_1 = pygame.mixer.music.play("Song.mp3")
-play.play()
+pygame.mixer_music.set_volume(0.8)
+print(pygame.mixer_music.get_volume())
+play_1 = pygame.mixer.music.play()
+pygame.mixer.music.load(".")
 while True:
     pass
