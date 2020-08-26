9074d552bb24f76bdfe377060214cc4066b8435d
Abhinav <abhinav.kamatamu@gmail.com>
2020-08-26 13:43:42 +0530

    message

---
+++ abhinav/for_fun/python/pygame/Genaral/Random_List.py
@@ -0,0 +1,5 @@
+
+for i in range(9+2):
+    if i % 2 == 1:
+        print(' ' * ((9 - i) // 2), '*' * i, ' ' * ((9 - i) // 2))
+print(i)
--- abhinav/for_fun/python/pygame/Songs/Beliver.py
+++ abhinav/for_fun/python/pygame/Songs/Beliver.py
@@ -3,10 +3,10 @@ from pygame.locals import *
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer_music.load("Song.mp3")
+pygame.mixer_music.load("/home/abhinav/Desktop/Abhi/abhinav/for_fun/python/pygame/Songs/Song.mp3")
 pygame.mixer_music.set_volume(0.8)
 print(pygame.mixer_music.get_volume())
 play_1 = pygame.mixer.music.play()
-pygame.mixer.music.load(".")
+pygame.mixer.music.load("/Song.mp3")
 while True:
     pass
