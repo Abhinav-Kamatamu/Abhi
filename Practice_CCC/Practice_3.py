c535000c9a4d426dbf29bb7fb622eba845d32df9
Abhinav <abhinav.kamatamu@gmail.com>
2024-11-27 01:50:12 +0530

    CCC practice day next?

--- Practice_CCC/Practice_3.py
+++ Practice_CCC/Practice_3.py
@@ -27,3 +27,52 @@ for input in data:
 print(winner[2], how_many[2])
 '''
 
+
+def find_troublesome_keys(key_presses, screen_output):
+    silly_key = None
+    wrong_letter = None
+    quiet_key = None
+
+    # We will use dictionaries to track occurrences
+    pressed = {}  # Dictionary to track characters that were pressed
+    displayed = {}  # Dictionary to track characters that were displayed
+
+    for key, display in zip(key_presses, screen_output):
+        if key != display:  # Possible silly key
+            if silly_key is None:
+                silly_key = key
+                wrong_letter = display
+            elif key == silly_key and display == wrong_letter:
+                continue  # Continue if we see the same mismatch
+            else:
+                # If different, this may indicate the wrong key setup, but not a concern in this task
+                pass
+
+        # Track the quiet key if no output is shown for a key
+        if display == '-':
+            if quiet_key is None and key != silly_key:
+                quiet_key = key
+
+    # Output the results
+    print(silly_key, wrong_letter)
+    if quiet_key:
+        print(quiet_key)
+    else:
+        print('-')
+
+
+# Sample Input 1
+key_presses1 = "forloops"
+screen_output1 = "fxrlxxps"
+find_troublesome_keys(key_presses1, screen_output1)
+
+# Sample Input 2
+key_presses2 = "forloops"
+screen_output2 = "fxrlxxp"
+find_troublesome_keys(key_presses2, screen_output2)
+
+# Sample Input 3
+key_presses3 = "forloops"
+screen_output3 = "frlpz"
+find_troublesome_keys(key_presses3, screen_output3)
+
