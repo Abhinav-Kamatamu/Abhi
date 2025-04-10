79d86d155370b8b1c4f943f3442c1166ffcd77d2
Abhinav-Kamatamu <abhinav.kamatamu@gmail.com>
2025-04-10 19:41:51 +0530

    Latest Version

--- abhinav/for_fun/Cue_Math/card_1455.py
+++
@@ -1,4 +0,0 @@
-coun = []
-listab = []
-
-print(listab[-1])
--- cv2(Someting).py
+++
@@ -1,9 +0,0 @@
-import cv2
-cap = cv2.VideoCapture(0)
-if not (cap.isOpened()):
-    print("Couldn't a;skdfj")
-frame = cap.read()
-cv2.imwrite('webcamphoto.jpg', frame)
-cap.release()
-
-
